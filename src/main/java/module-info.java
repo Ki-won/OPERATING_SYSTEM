6072ed9dec7f1b8edfd2cde15a178798bb30308c
Ki-won <seokw2240@gmail.com>
2023-05-10 15:20:59 +0900

    fix module- info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,9 @@ module com.koreatech.ifteam.operating_system {
             
     opens com.koreatech.ifteam.operating_system to javafx.fxml;
     exports com.koreatech.ifteam.operating_system.model;
-    exports com.koreatech.ifteam.operating_system.UI;
-    opens com.koreatech.ifteam.operating_system.UI to javafx.fxml;
+    exports com.koreatech.ifteam.operating_system.View;
+    opens com.koreatech.ifteam.operating_system.View to javafx.fxml;
     exports com.koreatech.ifteam.operating_system.model.packet;
+    exports com.koreatech.ifteam.operating_system.View.Data;
+    opens com.koreatech.ifteam.operating_system.View.Data to javafx.fxml;
 }
